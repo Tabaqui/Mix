074dcec9d91f7161f99c717fc48b88f870bd407b
Tabaqui <tabaqui.vn@gmail.com>
2016-03-18 07:42:46 +0300

    yn mvp

--- YetNotMVP/app/src/main/java/ru/motleycrew/yetnotmvp/MainActivity.java
+++ YetNotMVP/app/src/main/java/ru/motleycrew/yetnotmvp/MainActivity.java
@@ -1,13 +1,35 @@
 package ru.motleycrew.yetnotmvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ActivityMain";
+
+    private CallbackManager mCallbackManager;
+    private LoginButton mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext(), new FacebookSdk.InitializeCallback() {
+            @Override
+            public void onInitialized() {
+                mLoginButton = (LoginButton) findViewById(R.id.login_button);
+                mLoginButton.setReadPermissions("user_friends");
+            }
+        });
+        mCallbackManager = CallbackManager.Factory.create();
     }
+
+
 }
